320ab2cf450b7af8df3374cf6ec60e5f4c507c40
Imre Cserhati <cserhati.imre.andras@gmail.com>
2025-07-07 16:21:55 +0200

    uibuild 2025-07-07 16:21:55

--- index.js
+++ index.js
@@ -1,25 +1,8 @@
+require('dotenv').config()
 const express = require('express')
+const Note = require('./models/note')
 
 const app = express()
-
-let notes = [
-  {
-    id: '1',
-    content: 'HTML is easy',
-    important: true,
-  },
-  {
-    id: '2',
-    content: 'Browser can execute only JavaScript',
-    important: false,
-  },
-  {
-    id: '3',
-    content:
-      'GET and POST are the most important methods of HTTP protocol',
-    important: true,
-  },
-]
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -38,7 +21,9 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then((notes) => {
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
@@ -54,9 +39,7 @@ app.get('/api/notes/:id', (request, response) => {
 
 const generateId = () => {
   const maxId =
-    notes.length > 0
-      ? Math.max(...notes.map((n) => Number(n.id)))
-      : 0
+    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0
   return String(maxId + 1)
 }
 
@@ -93,7 +76,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
