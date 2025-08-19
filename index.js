eaaef616df3b7a81b85669897cec06e087f13e8e
Imre Cserhati <cserhati.imre.andras@gmail.com>
2025-08-19 16:34:25 +0200

    Modify noteSchema and catch errors in post route handler

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.static('dist'))
 app.use(express.json())
 app.use(requestLogger)
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
   if (!body.content) {
@@ -30,10 +30,13 @@ app.post('/api/notes', (request, response) => {
     important: body.important || false,
   })
 
-  note.save().then((savedNote) => {
-    console.log('savedNote', savedNote)
-    response.json(savedNote)
-  })
+  note
+    .save()
+    .then((savedNote) => {
+      console.log('savedNote', savedNote)
+      response.json(savedNote)
+    })
+    .catch((error) => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
@@ -96,6 +99,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
